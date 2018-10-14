13ba030fa433d6cde8ef8bc28af016829aef253a
ThiagoGomesOliveira <thiago.blu1988@gmail.com>
2018-10-14 17:35:54 -0300

    Strings

---
+++ ByteBank/ByteBank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "pagina?Argumentos";
+            string argumentos = url.Substring(7);
+            Console.WriteLine(argumentos);
+            Console.ReadLine();
+
+        }
+    }
+}
